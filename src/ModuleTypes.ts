9a094ea7175f9ac21d0ed41b32e2e345e4d40539
Chris <cl5522@ic.ac.uk>
2025-09-30 16:55:35 +0100

    departmental json and types

---
+++ src/ModuleTypes.ts
@@ -0,0 +1,31 @@
+type Weekday = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
+
+export type Interval = {
+  start: number;
+  end: number;
+};
+
+export type ModuleInfo = {
+  time: Interval;
+  week: number;
+  module: string;
+  room: string;
+  staff: string;
+  groups: string;
+  event_category: string;
+  published_online_link?: string;
+  department: string;
+  notes?: string;
+  id: string;
+};
+
+type WeeklyTimetable = Record<Weekday, ModuleInfo[]>;
+
+// trusting the JSON structure we should just do:
+
+// import fs from 'fs';
+// const computingJSON = fs.readFileSync('computing.json', 'utf-8');
+// const computingTimeTable: WeeklyTimetable = JSON.parse(computingJSON);
+
+// *optional try catch block maybe?
+// maybe we can just make a folder of different 
